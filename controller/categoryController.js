15da0dd4f4eab9cc0c3cee72396e41a475a3cc2d
Hibbanur-Rahman <hibbanrahmanhyt@gmail.com>
2024-02-26 18:29:46 +0530

    adding the controllers function of product and category

--- controller/categoryController.js
+++ controller/categoryController.js
@@ -3,11 +3,38 @@ const CategoryModel = require('../models/categoryModel');
 
 const AddCategory = async (req, res) => {
     try {
+        const { name } = req.body;
+        if (!name) {
+            return res.status(httpStatusCode.BAD_REQUEST).json({
+                success: false,
+                message: "name feild is empty"
+            })
+        }
+
+        const existingCategory = await CategoryModel.findOne({ name });
+        if (existingCategory) {
+            return res.status(httpStatusCode.CONFLICT).json({
+                success: false,
+                message: "The category is already exist !!"
+            })
+        }
+
+
+        const category = await CategoryModel.create({
+            name
+        })
+
+        if(!category){
+            return res.status(httpStatusCode.METHOD_NOT_ALLOWED).json({
+                success: false,
+                message: "something wrong in the creation of category"
+            })
+        }
 
         return res.status(httpStatusCode.OK).json({
             success: true,
             message: "You created Successfully !!",
-
+            data: category
         })
     } catch (error) {
         return res.status(httpStatusCode.INTERNAL_SERVER_ERROR).json({
--- controller/productController.js
+++ controller/productController.js
@@ -1,23 +1,70 @@
 const httpStatusCode = require('../constant/httpStatusCode');
 const ProductModel = require('../models/productsModel');
+const { validationResult } = require('express-validator');
 
+const AddProduct = async (req, res) => {
+    try {
+        // Validate incoming request data
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(httpStatusCode.BAD_REQUEST).json({
+                success: false,
+                message: "Validation failed",
+                errors: errors.array()
+            });
+        }
 
-const AddProduct = async (req,res)=>{
-    try{
+        const { name, description, price, productImage, category } = req.body;
+
+        const product = await ProductModel.create({
+            name,
+            description,
+            price,
+            productImage,
+            category
+        });
+
+        if (!product) {
+            return res.status(httpStatusCode.NOT_FOUND).json({
+                success: false,
+                message: "Product is not created due to an error"
+            });
+        }
 
         return res.status(httpStatusCode.OK).json({
             success: true,
-            message:"add product successfully !!",
+            message: "Product added successfully!",
+            data: product
+        });
+    } catch (error) {
+        return res.status(httpStatusCode.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            message: "Something went wrong!",
+            error: error.message
+        });
+    }
+};
 
-        })
+const DeleteProduct= async (req,res)=>{
+    try{
+        const userId= req.user._id;
+
+        if(!userId){
+            return res.status(httpStatusCode.BAD_REQUEST).json({
+                success: false,
+                message:"user is not found"
+            })
+        }
+
+        
+        
     }catch(error){
         return res.status(httpStatusCode.INTERNAL_SERVER_ERROR).json({
             success: false,
-            message:"Something went wrong !!",
+            message: "Something went wrong!",
             error: error.message
-        })
+        });
     }
 }
 
-
-module.exports={AddProduct}
+module.exports = { AddProduct };
